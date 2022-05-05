9fe8b2ccd58235bce107e10a6519947e77d02715
tuutti <tuutti@iki.fi>
2022-05-05 16:07:39 +0300

    Return types

--- src/Plugin/Field/FieldType/PackageReferenceItem.php
+++ src/Plugin/Field/FieldType/PackageReferenceItem.php
@@ -15,31 +15,30 @@ use Drupal\Core\TypedData\DataDefinition;
  * Defines the 'pkg_status_version' field type.
  *
  * @FieldType(
- *   id = "pkg_status_version",
- *   label = @Translation("Version"),
+ *   id = "pkg_status_package_reference",
+ *   label = @Translation("Package reference"),
  *   category = @Translation("Package status"),
  *   default_widget = "string_textfield",
  *   default_formatter = "string"
  * )
  */
-final class VersionItem extends FieldItemBase {
+final class PackageReferenceItem extends FieldItemBase {
 
   /**
    * {@inheritdoc}
    */
   public function isEmpty() : bool {
-    $value = $this->get('value')->getValue();
-    return $value === NULL || $value === '';
+    return !$this->get('package_id')->getValue();
   }
 
   /**
    * {@inheritdoc}
    */
   public static function propertyDefinitions(FieldStorageDefinitionInterface $field_definition) : array {
-    $properties['value'] = DataDefinition::create('string')
-      ->setLabel(new TranslatableMarkup('Version'))
+    $properties['id'] = DataDefinition::create('integer')
+      ->setLabel(new TranslatableMarkup('Package id'))
       ->setRequired(TRUE);
-    $properties['status'] = DataDefinition::create('integer')
+    $properties['version'] = DataDefinition::create('integer')
       ->setLabel(new TranslatableMarkup('Status'))
       ->setRequired(TRUE);
 
@@ -49,33 +48,21 @@ final class VersionItem extends FieldItemBase {
   /**
    * {@inheritdoc}
    */
-  public function getConstraints() : array {
-    $constraints = parent::getConstraints();
-
-    $constraint_manager = \Drupal::typedDataManager()->getValidationConstraintManager();
-    $options['value']['Length']['max'] = 255;
-    $constraints[] = $constraint_manager->create('ComplexData', $options);
-    return $constraints;
-  }
-
-  /**
-   * {@inheritdoc}
-   */
   public static function schema(FieldStorageDefinitionInterface $field_definition) : array {
     return [
       'columns' => [
-        'value' => [
-          'type' => 'varchar',
-          'not null' => FALSE,
-          'length' => 255,
-        ],
-        'status' => [
+        'id' => [
           'type' => 'int',
-          'size' => 'tiny',
+          'size' => 'big',
+        ],
+        'version' => [
+          'type' => 'varchar',
+          'not null' => TRUE,
+          'length' => 255,
         ],
       ],
       'indexes' => [
-        'value' => ['value'],
+        'value' => ['id', 'version'],
       ],
     ];
   }
@@ -84,9 +71,10 @@ final class VersionItem extends FieldItemBase {
    * {@inheritdoc}
    */
   public static function generateSampleValue(FieldDefinitionInterface $field_definition) : array {
-    $random = new Random();
-    $values['value'] = $random->word(mt_rand(1, 50));
-    return $values;
+    return [
+      'id' => random_int(1, 500),
+      'version' => '1.0.0',
+    ];
   }
 
 }
